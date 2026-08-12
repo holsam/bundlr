6c4ef4c4d4d10f1f3e76590fe8568aa4ca575b2c
Sam Holland <hol_sam@icloud.com>
2026-08-12 16:12:08 +0100

    refactor: move constants to a defaults.py file

---
+++ src/bundlr/defaults.py
@@ -0,0 +1,12 @@
+'''
+Default bundlr constants
+'''
+
+# Define directories to ignore during bundling
+IGNORE_DIRS = {'.git', 'node_modules', 'dist', 'build', '__pycache__', '.venv', 'venv'}
+
+# Define files to ignore during bundling
+IGNORE_FILES = {'.DS_Store'}
+
+# Define extensions of binary files
+BINARY_EXTENSIONS = {'.png', '.jpg', '.jpeg', '.gif', '.bmp', '.ico', '.pdf', '.zip', '.tar', '.gz', '.mp4', '.mp3', '.woff', '.woff2', '.ttf', '.eot'}
